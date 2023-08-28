3cd50ebbc117a9ab1f2f834cda01a7ce518629a8
Shivam Kumar <shivambaku@gmail.com>
2023-08-29 00:17:48 +0400

    chore: improved hover animation

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -8,6 +8,6 @@ defineProps<{
 
 <template>
   <div class="fixed bottom-[100px] flex h-[180px] w-[120px] items-center justify-center">
-    <img :src="`/src/assets/cards/${card.img}`" :alt="card.name" class="max-h-full max-w-full hover:mb-20">
+    <img :src="`/src/assets/cards/${card.img}`" :alt="card.name" class="max-h-full max-w-full hover:-translate-y-10 hover:scale-110">
   </div>
 </template>
