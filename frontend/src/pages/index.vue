0d923f739bc7a44f88ccbbae8c686a0a9bce83aa
Shivam Kumar <shivambaku@gmail.com>
2023-09-03 15:22:38 -0700

    chore: adjusted tailwind for main page

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -3,25 +3,17 @@ import GameModeCard from '@/components/GameModeCard.vue';
 </script>
 
 <template>
-  <div class="h-screen w-screen">
-    <div class="flex h-screen flex-col">
-      <div class="flex h-1/4">
-        <div class="m-auto text-center text-4xl text-white">
-          <div>
-            Welcome
-          </div>
-          <div>
-            Choose your Mode
-          </div>
-        </div>
+  <div class="flex h-screen w-screen flex-col">
+    <div class="flex basis-4/12 items-center justify-center text-center text-4xl text-white">
+      <div>
+        <div>Welcome</div>
+        <div>Choose your Mode</div>
       </div>
-      <div class="h-1/2">
-        <div class="flex h-full items-center justify-center">
-          <GameModeCard :id="3" class="origin-bottom -rotate-12" title="3 Player" :players-count="30" :rooms-count="23" />
-          <GameModeCard :id="4" class="-translate-y-6" title="4 Player" :players-count="43" :rooms-count="12" />
-          <GameModeCard :id="5" class="origin-bottom rotate-12" title="5 Player" :players-count="45" :rooms-count="33" />
-        </div>
-      </div>
+    </div>
+    <div class="mx-20 mb-20 flex basis-8/12 items-center justify-evenly text-center">
+      <GameModeCard :id="3" class="origin-bottom -rotate-12" title="3 Player" :players-count="30" :rooms-count="23" />
+      <GameModeCard :id="4" class="-translate-y-6" title="4 Player" :players-count="43" :rooms-count="12" />
+      <GameModeCard :id="5" class="origin-bottom rotate-12" title="5 Player" :players-count="45" :rooms-count="33" />
     </div>
   </div>
 </template>
