28871d818a2472df4747cb498fd603f6940098e0
Shivam Kumar <shivambaku@gmail.com>
2023-09-12 23:37:50 -0700

    fix: the branch

--- frontend/src/pages/desk/3_p.vue
+++ frontend/src/pages/desk/3_p.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
-import type { VueElement } from 'vue';
-import { computed, ref } from 'vue';
+import { computed, onBeforeMount } from 'vue';
+import { Icon } from '@iconify/vue';
 import Card from '@/components/Card.vue';
 import Player from '@/components/Player.vue';
 import { useJudgementGame } from '@/composables/useJudgementGame';
 
-const { gameState, newRound, roundFinished } = useJudgementGame(3);
+const { gameState, currentRound, dealCards, finishRound } = useJudgementGame(4);
+// Change player count
 
-newRound();
+onBeforeMount(() => {
+  dealCards();
+});
 
 function calculateRotationClass(playerIndex: number, index: number) {
-  const rotation = index * 10 - (gameState.value.players[playerIndex].cards.length - 1) * 10 / 2;
-
+  const rotation = index * 10 - (currentRound.value.hands[playerIndex].cards.length - 1) * 10 / 2;
   return `rotate-[${rotation}deg]`;
 }
 
@@ -19,71 +21,57 @@ function calculateTranslationClass(_playerIndex: number, _index: number) {
   return '';
 }
 
-const roundFinishedButtonClass = computed(() => {
+const newRoundButtonClass = computed(() => {
   return gameState.value.gameFinished ? 'bg-gray-500' : 'bg-red-500';
 });
 
-const draggedElementRef = ref<HTMLElement | null>(null);
+// const setBid = computed(() => {
 
-function allowDrop(ev: DragEvent): void {
-  ev.preventDefault();
-}
+// });
 
-function drag(ev: DragEvent): void {
-  const targetElement = ev.target as HTMLElement;
-  ev.dataTransfer!.setData('text', targetElement.id);
-  draggedElementRef.value = targetElement;
-}
-
-function drop(ev: DragEvent): void {
-  ev.preventDefault();
-  const targetElement = ev.target as HTMLElement;
-  const draggedElement = draggedElementRef.value;
-
-  if (draggedElement)
-    targetElement.appendChild(draggedElement);
-
-  draggedElementRef.value = null;
+function newRound() {
+  finishRound();
+  dealCards();
 }
 </script>
 
 <template>
-  <div class="flex h-screen w-screen items-end bg-gray-800 pb-5 ">
-    <div
-      class=" fixed left-1/4 top-[300px] flex  h-1/4 w-2/4 border-2  border-white p-1" @drop="drop"
-      @dragover="allowDrop"
-    />
+  <div class="flex h-screen w-screen items-end overflow-hidden">
     <div class="relative flex h-1/3 w-full items-center justify-center">
-      <Card
-        v-for="(card, index) in gameState.players[0].cards" id="drag1" :key="card.img"
-        :card="card" class="absolute origin-bottom" :class="[calculateRotationClass(0, index), calculateTranslationClass(0, index)]" @dragstart="drag"
-      />
+      <div class="absolute top-0 z-50 flex items-center  justify-between">
+        <Icon :class="currentRound.currentCallPlayer.id === gameState.players[0].id ? 'text-green-500' : 'text-black'" icon="ic:twotone-circle" />
+        <p class=" pl-1">
+          [ 0 / 3 ]
+        </p>
+      </div>
+      <Card v-for="(card, index) in currentRound.hands[0].cards" :key="card.img" player="main" :card="card" class="absolute  origin-bottom " :class="[calculateRotationClass(0, index), calculateTranslationClass(0, index)]" />
     </div>
-    <div class="fixed left-2 top-[120px] flex  h-28 w-1/4 rotate-180 items-center justify-center">
-      <Player :id="gameState.players[1].id" name="Player 1" :tricks="0" :bid="3" :score="120" class="rotate-180" />
-      <Card
-        v-for="(card, index) in gameState.players[1].cards" :key="card.img" :card="card"
-        class="absolute !h-[60px]  !w-[40px] origin-bottom" :class="calculateRotationClass(1, index)" @dragstart="drag"
-      />
+    <div class="fixed  left-[8%]  top-[100px] flex h-28 w-1/4 rotate-180 items-center justify-center">
+      <Player :id="gameState.players[1].id" :turn="currentRound.currentCallPlayer.id === gameState.players[1].id ? 'text-green-500' : 'text-black'" name="Player 2" :tricks="0" :bid="3" :score="120" class="rotate-180" />
+      <Card v-for="(card, index) in currentRound.hands[1].cards" :key="card.img" player="other" :card="card" class="absolute -top-4 h-[60px] w-[40px] origin-bottom" :class="calculateRotationClass(1, index)" />
     </div>
-    <div class="fixed right-2 top-[120px] flex h-28 w-1/4 rotate-180 items-center justify-center">
-      <Player :id="gameState.players[2].id" name="Player 2" :tricks="0" :bid="3" :score="120" class="rotate-180" />
-      <Card
-        v-for="(card, index) in gameState.players[2].cards" :key="card.img" :card="card"
-        class="absolute !h-[60px] !w-[40px] origin-bottom" :class="calculateRotationClass(1, index)" @dragstart="drag"
-      />
+    <div class="fixed right-[8%] top-[100px] flex  h-28 w-1/4 rotate-180 items-center justify-center">
+      <Player :id="gameState.players[2].id" :turn="currentRound.currentCallPlayer.id === gameState.players[2].id ? 'text-green-500' : 'text-black'" name="Player 3" :tricks="0" :bid="3" :score="120" class="rotate-180" />
+      <Card v-for="(card, index) in currentRound.hands[2].cards" :key="card.img" player="other" :card="card" class="absolute -top-4 h-[60px] w-[40px] origin-bottom" :class="calculateRotationClass(1, index)" />
     </div>
-    <button
-      :disabled="gameState.gameFinished" class="fixed right-0 top-0 p-2 text-white"
-      :class="roundFinishedButtonClass" @click="roundFinished"
-    >
-      Finish Round
+    <button :disabled="gameState.gameFinished" class="fixed right-0 top-0 p-2 text-white" :class="newRoundButtonClass" @click="newRound">
+      New Round
     </button>
-    <div class="fixed top-0 h-[60px] w-[40px] flex-row items-center justify-center">
-      <p class=" text-center text-xs  text-white">
+    <div class="fixed right-0 top-[37%]  m-2 h-[80px] w-[40px] flex-row items-center justify-center">
+      <p class=" text-center text-xs text-white">
         TRUMP
       </p>
-      <img :src="`/src/assets/cards/${gameState.currentTrump}.svg`" alt="" class="max-h-full max-w-full">
+      <img :src="`/src/assets/cards/${currentRound.currentTrump}.svg`" alt="" class="max-h-full max-w-full">
+    </div>
+    <div class="fixed left-0 top-[35%]  m-2  h-[120px] w-[80px] flex-row items-center justify-center border-2 border-white sm:h-[160px] sm:w-[140px]">
+      <p class=" mb-1 text-center text-[12px]  text-white sm:text-[16px]">
+        Total score
+      </p>
+      <ul class=" text-center text-[12px]  text-white sm:text-[16px]">
+        <li v-for="playerScore in gameState.players" :key="playerScore.id">
+          {{ playerScore.name }} - {{ playerScore.score }}
+        </li>
+      </ul>
     </div>
   </div>
 </template>
